79056c213b5a1706112ebf971a5dc23282a001f2
Harsh Gajera <harsh.gajera17@gmail.com>
2022-04-15 20:29:19 +0530

    all doner

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Harsh");
+let add = require("./add");
+let sub = require("./sub");
+let mul = require("./mul");
+let div = require("./div");
+
+console.log(add(10, 15));
+console.log(sub(10, 15));
+console.log(mul(10, 15));
+console.log(div(10, 15));
+
+console.log(add(100, 5));
+console.log(sub(100, 5));
+console.log(mul(100, 5));
+console.log(div(100, 5));
